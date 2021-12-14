7903b13a62ed2c582c0323ec1e6810fa440accd3
Antoine209 <73525571+Antoine209@users.noreply.github.com>
2021-12-14 23:06:31 +0100

    Modification des pages contact et royalFamily Ajout d'une mise en page pour la page contact. Tentatives d'un jeu de cartes dans la page royalFamily

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const message =
   "Thank you for submitting your message to the Argo ship. Jason and his crew will respond shortly.";
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
+
+let contactForm = document.getElementById("contactForm");
+if (contactForm !== null) {
+  contactForm.addEventListener("submit", function (event) {
     event.preventDefault();
     alert(message);
   });
+}
+
+let boxes = document.getElementsByClassName("box");
+if (boxes !== null) {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("mouseenter", function (event) {
+      console.log("enter");
+      console.log(event.target);
+      event.target.children[0].children[0].hidden = true;
+    });
+    boxes[i].addEventListener("mouseover", function (event) {
+      console.log("over");
+      console.log(event.target);
+      event.target.children[0].children[0].hidden = false;
+    });
+  }
+}
